68454faf9c7e7a7e152f034e6524810f32affd7a
Romil Movaliya <romilmovaliya2929@gmail.com>
2023-06-20 19:57:30 +0530

    here i add 180 Degree rotation half

--- inverted_halfPyramid.java
+++ inverted_halfPyramid.java
@@ -1,18 +1,44 @@
 public class inverted_halfPyramid {
     public static void main(String[] args) {
-        
 
-         //rows
-        for(int i=0;i<4;i++){
+        /*
+         * //rows
+         * for(int i=0;i<4;i++){
+         * 
+         * //colmns
+         * for(int j=4;j>i;j--){
+         * 
+         * System.out.print("*");
+         * 
+         * }
+         * System.out.println();
+         * 
+         * }
+         */
 
-            //colmns
-            for(int j=4;j>i;j--){
+        /*
+         * 
+         * _,_,*
+         * _,*,*
+         * ,*,*
+         * 
+         * 
+         */
 
-                 System.out.print("*");   
-                
+        // inverted half pyramid for 180% rotate
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < (3 - i); j++) {
+                System.out.print(" ");
             }
-             System.out.println();   
-        
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+                
+            System.out.println();
         }
+        System.out.println();
     }
 }
