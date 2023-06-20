8177ac026703a24530a71950ae12dbb5b34b3e23
Romil Movaliya <romilmovaliya2929@gmail.com>
2023-06-20 19:32:44 +0530

    this is my third commit

---
+++ inverted_halfPyramid.java
@@ -0,0 +1,18 @@
+public class inverted_halfPyramid {
+    public static void main(String[] args) {
+        
+
+         //rows
+        for(int i=0;i<4;i++){
+
+            //colmns
+            for(int j=4;j>i;j--){
+
+                 System.out.print("*");   
+                
+            }
+             System.out.println();   
+        
+        }
+    }
+}
