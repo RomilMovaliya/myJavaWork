a7ee2faebdd3af67ab86cf2d60ffea78d6156a33
Romil Movaliya <romilmovaliya2929@gmail.com>
2023-06-20 19:35:47 +0530

    this is my half_pyramid.java file is adding

---
+++ half_pyramid.java
@@ -0,0 +1,17 @@
+public class half_pyramid {
+    public static void main(String[] args) {
+        
+        //rows
+        for(int i=0;i<4;i++){
+
+            //colmns
+            for(int j=0;j<=i;j++){
+
+                 System.out.print("*");   
+                
+            }
+             System.out.println();   
+        
+        }
+    }
+}
